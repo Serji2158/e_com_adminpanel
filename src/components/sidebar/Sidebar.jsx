77ae39230b2521b3a175559631fb12893a26897e
Sergiy Tyschenko <sergey.tyschenko@gmail.com>
2022-09-30 18:46:15 +0300

    sidebar,navbar,widgets added

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,16 @@
 import "./sidebar.scss"
+import InsertChartOutlinedIcon from '@mui/icons-material/InsertChartOutlined';
+import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined';
+import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
+import CreditCardOutlinedIcon from '@mui/icons-material/CreditCardOutlined';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+import QueryStatsOutlinedIcon from '@mui/icons-material/QueryStatsOutlined';
+import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
+import HealthAndSafetyOutlinedIcon from '@mui/icons-material/HealthAndSafetyOutlined';
+import SettingsInputAntennaOutlinedIcon from '@mui/icons-material/SettingsInputAntennaOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import ContactPageOutlinedIcon from '@mui/icons-material/ContactPageOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 const Sidebar = () => {
   return (
@@ -9,14 +21,66 @@ const Sidebar = () => {
       <hr />    
       <div className="center">
         <ul>
-          <li><span>Dashboard</span></li>
-          <li><span>Dashboard</span></li>
-          <li><span>Dashboard</span></li>
-          <li><span>Dashboard</span></li>
-  
+          <p className="title">MAIN</p>
+          <li>
+            <InsertChartOutlinedIcon className="icon"/>
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <li>
+            <ManageAccountsOutlinedIcon className="icon"/>
+            <span>Users</span>
+          </li>
+          <li>
+            <Inventory2OutlinedIcon className="icon"/>
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCardOutlinedIcon className="icon"/>
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingOutlinedIcon className="icon"/>
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL LINKS</p>
+          <li>
+            <QueryStatsOutlinedIcon className="icon"/>
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsActiveOutlinedIcon className="icon"/>
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <HealthAndSafetyOutlinedIcon className="icon"/>
+            <span>System Health</span>
+          </li>
+          <li>
+            <SettingsInputAntennaOutlinedIcon className="icon"/>
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsOutlinedIcon className="icon"/>
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <ContactPageOutlinedIcon className="icon"/>
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutOutlinedIcon className="icon"/>
+            <span>Logout</span>
+          </li>  
         </ul>
       </div>
-      <div className="bottom">color options</div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      
+      </div>
     </div>
   )
 }
