6b9335ce045f6572265dd8ae0f0545e0d7eec5c3
Sergiy Tyschenko <sergey.tyschenko@gmail.com>
2022-09-30 10:08:12 +0300

    structure added

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,24 @@
+import "./sidebar.scss"
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">LadmyAdmin</span>
+      </div>
+      <hr />    
+      <div className="center">
+        <ul>
+          <li><span>Dashboard</span></li>
+          <li><span>Dashboard</span></li>
+          <li><span>Dashboard</span></li>
+          <li><span>Dashboard</span></li>
+  
+        </ul>
+      </div>
+      <div className="bottom">color options</div>
+    </div>
+  )
+}
+
+export default Sidebar
